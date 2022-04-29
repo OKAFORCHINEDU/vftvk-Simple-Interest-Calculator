38b9f35681309d8a2c92ce2efc73eb5c9a33ba02
OKAFORCHINEDU <91702144+OKAFORCHINEDU@users.noreply.github.com>
2022-04-29 16:12:33 +0100

    Updated script.js to display percent

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function compute() {
 
 /*update the rate value*/
 function getSliderValue() {
-    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value + "%";
 }
 
 //Check for positive values
